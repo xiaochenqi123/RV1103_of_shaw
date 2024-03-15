779447057aeab41d40d2a554e47f5b36457734fa
Shaw123 <144078330+xiaochenqi123@users.noreply.github.com>
2024-03-15 21:40:10 +0800

    Add files via upload
    2.1版本，线程同步做的不太好，但是加了边界检查，在低采样率下可以工作

---
+++ myproject/caijiban2_1/sources/sd.c
@@ -0,0 +1,42 @@
+#include "sd.h"
+#include "udpsend.h"
+#include "adc.h"
+
+
+extern uint8_t WORKMODE;
+extern uint16_t flag_2;
+extern uint8_t buffer1[BUFFER_SIZE];  // 第一个缓冲区
+extern uint8_t buffer2[BUFFER_SIZE];  // 第二个缓冲区
+extern pthread_mutex_t flag_mutex;
+extern uint16_t flag;
+
+void sdsave(void)
+{
+    pthread_mutex_lock(&flag_mutex);
+    FILE *sd_file;
+    sd_file = fopen("/mnt/sdcard/test.txt", "a");
+    if (sd_file == NULL)
+    {
+        perror("Error opening SD card file");
+        exit(1);
+    }
+    while(1){
+        
+        if(flag==BUFFER_SIZE){
+            if (WORKMODE == SDMODE) {
+
+            if (flag_2 % 2 == 0) {
+                fwrite(buffer1, sizeof(uint8_t), BUFFER_SIZE, sd_file);
+                printf("write success\r\n");
+            } else {
+                fwrite(buffer2, sizeof(uint8_t), BUFFER_SIZE, sd_file);
+            }
+                fclose(sd_file);
+            }
+            flag = 0;
+            pthread_mutex_unlock(&flag_mutex);
+        }
+        pthread_mutex_unlock(&flag_mutex);
+    }
+
+}
