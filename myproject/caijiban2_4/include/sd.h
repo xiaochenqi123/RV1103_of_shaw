a1ed3677185f307ccd8ecd0049e215f6243ea950
Shaw123 <144078330+xiaochenqi123@users.noreply.github.com>
2024-03-18 17:05:36 +0800

    2.0系类最后一版
    已经实现了稳定的定时器，ADC，网络传输，SD卡存储，但是采样率低需要再调节一下，另外内存的问题也需要解决（RV1103有64MB内存，不可能怎么快就溢出）。

---
+++ myproject/caijiban2_4/include/sd.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <signal.h>
+#include <time.h>
+#include <stdint.h>
+#include <pthread.h>
+
+FILE *sd_file;
+
+#define SDMODE 1
+
+
+void sdsave(void);
---
+++ myproject/caijiban2_4/sources/udpsend.c
@@ -0,0 +1,58 @@
+// 接收来自上位机的数据包
+#include "udpsend.h"
+#include "adc.h"
+#include "sd.h"
+#include "main.h"
+    extern int sockfd;
+    extern uint16_t flag;
+    extern uint8_t WORKMODE;
+    extern uint8_t timer_flag;
+    extern uint16_t flag_2;
+    extern sem_t semaphore;
+    extern pthread_mutex_t flag_mutex;
+
+    extern uint8_t buffer1[BUFFER_SIZE];
+    extern uint8_t buffer2[BUFFER_SIZE];
+void udpsend(void)
+{
+        
+        pthread_mutex_lock(&flag_mutex);
+        flag = 0;
+        pthread_mutex_unlock(&flag_mutex);
+        while(1){
+            if(WORKMODE == UDPMODE)
+         {
+            if (sem_wait(&semaphore)==0) {
+                if (flag_2 % 2 == 0) {
+                    ssize_t bytes_sent = send(sockfd, buffer1, BUFFER_SIZE, 0);
+                    pthread_mutex_lock(&flag_mutex);
+                    flag = 0;
+                    pthread_mutex_unlock(&flag_mutex);
+                    //printf("send buffer_1\r\n");
+                    flag_2++;
+                    if (bytes_sent == -1)
+                    {
+                        perror("send");
+                        exit(1);
+                    }
+                    
+                } else {
+                    ssize_t bytes_sent = send(sockfd, buffer2, BUFFER_SIZE, 0);
+                    pthread_mutex_lock(&flag_mutex);
+                    flag = 0;
+                    pthread_mutex_unlock(&flag_mutex);
+                    //printf("send buffer_2\r\n");
+                    flag_2--;
+                    if (bytes_sent == -1)
+                    {
+                        perror("send");
+                        exit(1);
+                    }
+                    
+                }
+            } 
+            pthread_mutex_unlock(&flag_mutex);
+        } 
+           // usleep(1000); // sleep for 1ms to prevent busy waiting
+        }
+}
